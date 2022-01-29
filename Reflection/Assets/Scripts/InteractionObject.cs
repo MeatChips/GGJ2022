39d633d7097a31acf84c9f96997b52e54df6e261
AlexBlok1083 <alexblok50@gmail.com>
2022-01-30 00:20:52 +0100

    .

---
+++ Reflection/Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    void OnMouseClick()
+    {
+        Debug.Log("Yes");
+    }
+}
